d6906d2d8fbca6cbafeafb811bd3fbc3cce7d846
Chunliang Lu <aptenom@gmail.com>
2012-11-04 06:25:08 -0800

    the overall structure looks fine now.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -93,28 +93,7 @@
     <?php endif; ?>
 
     <div id="navigation">
-
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see http://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
+ 
       <?php print render($page['navigation']); ?>
 
     </div><!-- /#navigation -->
@@ -150,6 +129,7 @@
     <?php endif; ?>
 
     <?php print render($page['header']); ?>
+  
 
   </header>
 
@@ -177,7 +157,6 @@
   </div><!-- /#main -->
 
   <?php print render($page['footer']); ?>
-
 </div><!-- /#page -->
 
 <?php print render($page['bottom']); ?>
